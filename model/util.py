d7173d08ea0bc2466e18d0b2f64e78496e9466b3
Mrpatekful <purgai.patrik@gmail.com>
2017-10-15 13:31:22 +0200

    util

--- model/util.py
+++ model/util.py
@@ -23,3 +23,40 @@ class Stroke:
         self.start_time = start_time
         self.end_time = end_time
         self.horizontal = False
+
+
+def point_2_point(point_a, point_b):
+    """
+    Distance between two points.
+    :param point_a: First point.
+    :param point_b: Second point.
+    :return: Distance of the points.
+    """
+    return math.sqrt((point_a.x - point_b.x) ** 2 + (point_a.y - point_b.y) ** 2)
+
+
+def point_2_line(point_a, point_b, point):
+    """
+    Distance between a point and a line.
+    :param point_a: First point of the vector.
+    :param point_b: Second point of the vector.
+    :param point: The point, which distance is to be measured from the line.
+    :return: Distance of the point from the line.
+    """
+    return (math.fabs(-(point_b.y - point_a.y) * point.x + (point_b.x - point_a.x) * point.y +
+                      (point_b.y - point_a.y) * point_a.x -
+                      (point_b.x - point_a.x) * point_a.y)) / math.sqrt(
+        (point_b.x - point_a.x) ** 2 + (point_b.y - point_a.y) ** 2)
+
+
+def calculate_angle(point_a, point_b):
+    """
+    Calculates the included angle (degrees) of a vector and horizontal line.
+    :param point_a: First point of the vector.
+    :param point_b: Second point.
+    :return: The included angle (degrees) of the vector, defined by the points,
+     and a horizontal line.
+    """
+    vector = Point(point_b.x - point_a.x, point_b.y - point_a.y)
+    n_vector = Point(vector.x / vector.length, vector.y / vector.length)
+    return math.degrees(math.acos(n_vector.x))
