3002367cae92d5719541a9ef0239103751b0c391
Csutorás Robin <csutorasr@hotmail.com>
2017-10-16 17:39:02 +0200

    make title smaller after load

--- web-interface-sln/web-interface/src/app/app.component.ts
+++ web-interface-sln/web-interface/src/app/app.component.ts
@@ -1,9 +1,18 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  small$: Observable<boolean>;
+  constructor(private router: Router) { }
+  ngOnInit(): void {
+    this.small$ = this.router.events.filter(x => x instanceof NavigationEnd)
+      .map(x => (x as NavigationEnd).url !== '/');
+  }
+
 }
