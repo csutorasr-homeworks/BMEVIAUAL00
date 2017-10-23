1182f4f9023f86c0b7a95cc03e3fc7e7af042dac
Mrpatekful <purgai.patrik@gmail.com>
2017-10-23 13:40:10 +0200

    mlp

--- model/func.py
+++ model/func.py
@@ -1,4 +1,3 @@
-import math
 import numpy as np
 
 
@@ -6,9 +5,36 @@ def tangent_hyperbolic(x):
     return np.exp(x)-np.exp(-x)/(np.exp(x) + np.exp(-x))
 
 
+def d_tangent_hyperbolic(x):
+    return x
+
+
 def sigmoid(x):
     return 1/(1 - np.exp(-x))
 
 
+def d_sigmoid(x):
+    return x
+
+
 def relu(x):
     return x[x > 0]
+
+
+def d_relu(x):
+    return x
+
+
+def linear(x):
+    return x
+
+
+def d_linear(x):
+    return x
+
+
+functions = {tangent_hyperbolic: d_tangent_hyperbolic,
+             sigmoid: d_sigmoid,
+             relu: d_relu,
+             linear: d_linear}
+
--- model/mlp.py
+++ model/mlp.py
@@ -1,23 +1,42 @@
 import numpy as np
+import func
 
 
 class Dense:
 
-    def __init__(self, units, input_dim=None, activation=None):
+    def __init__(self, units, input_dim=None, activation=func.linear):
+
         self.input_dim = input_dim + 1
         self.units = units
-        self.activation = activation
+
+        self.activation = Activation(activation)
+        self.d_activation = func.functions[activation]
+
         self.weights = np.random.random((input_dim, units))
         self.dw = np.zeros(self.weights.shape)
 
-    def output(self, input_data):
-        return self.activation.function(input_data, self.weights)
+        self.output = None
+
+        self.dropout = None
+        self.batch_normalization = None
+
+    def propagate_forward(self, input_data):
+        self.output = self.activation.function(input_data, self.weights)
 
     def modify_weights(self, learning_rate, delta):
-        self.weights += learning_rate*delta
+        self.weights -= learning_rate*np.dot(self.output.T, delta)
 
-    def propagate_backward(self):
-        pass
+    def add(self, activation=None, dropout=None, batch_normalization=None):
+
+        if activation is not None:
+            self.activation = activation
+            self.d_activation = func.functions[activation]
+
+        if dropout is not None:
+            self.dropout = dropout
+
+        if batch_normalization is not None:
+            self.batch_normalization = batch_normalization
 
 
 class Activation:
@@ -35,19 +54,58 @@ class Dropout:
         pass
 
 
+class BatchNorm:
+
+    def __init__(self):
+        pass
+
+
 class Sequential:
 
     def __init__(self):
         self.layers = []
 
     def add(self, layer):
-        self.layers.append(layer)
 
-    def fit(self, x_train, y_train, epochs, batch_size):
-        pass
+        if type(layer) is Activation:
+            self.layers[-1].add(activation=layer)
+
+        elif type(layer) is Dropout:
+            self.layers[-1].add(dropout=layer)
+
+        elif type(layer) is BatchNorm:
+            self.layers[-1].add(batch_normalization=layer)
+
+        else:
+            self.layers.append(layer)
+
+    def fit(self, x_train, y_train, epochs = 100, batch_size):
+
+        deltas = []
+
+        for epoch in epochs
+
+        input_data =
+        for layer in self.layers:
+            layer.propagate_forward(input_data)
+            input_data = layer.output
+
+
+
+        for layer in self.layers[::-1]:
+            pass
 
     def compile(self, loss, optimizer):
         pass
 
     def predict(self, batch):
         pass
+
+
+def main():
+    model = Sequential()
+    model.add(Dense(units=4, input_dim=4))
+
+
+if __name__ == "__main__":
+    main()
